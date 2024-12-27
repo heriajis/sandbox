df4343111c5009a1c8923b900b2d0d867a14615b
Heri Aji Setiawan <heriajisetiawan@Heris-Air.lan>
2024-12-27 19:04:11 +0700

    update bot

--- sandbot-bot.py
+++ sandbot-bot.py
@@ -20,9 +20,15 @@ def scrape_and_notify():
     # Membuat session untuk menjaga cookie dan sesi
     session = requests.Session()
 
-    # Menambahkan header User-Agent untuk mensimulasikan permintaan dari browser
+    # Menambahkan header User-Agent dan header lainnya untuk mensimulasikan browser biasa
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "DNT": "1"  # Do Not Track
     }
 
     # Mengirim permintaan GET dengan sesi dan header User-Agent
